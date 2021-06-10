b6ae397f16bfb0fb5f220d21bf6ca51d7aa1da7f
Javier Ocampo <ferroxido@gmail.com>
2021-06-09 22:43:10 -0300

    WIP: Team section

--- src/components/Team/index.tsx
+++ src/components/Team/index.tsx
@@ -1,30 +1,67 @@
 import React from 'react';
-import { css } from '@emotion/css';
-import { Box, Flex } from '@chakra-ui/react';
-
-const arrowDownCss = css({
-  width: 0,
-  height: 0,
-  borderTop: '50px solid #2b6cb0',
-  borderLeft: '50px solid transparent',
-  borderRight: '50px solid transparent',
-  position: 'absolute',
-  left: '50%',
-  top: '100px',
-  transform: 'translate(-50%, 0)'
-});
+import { Box, Flex, Image } from '@chakra-ui/react';
+import BackgroundImage from '../../assets/panel.jpg';
 
 function Team(): JSX.Element {
   return (
-    <Flex>
+    <Flex direction="column">
       <Box
         background="blue.600"
-        height="100px"
+        height={["30px", "60px", "90px"]}
         width="100%"
         position="relative"
       >
-        <Box className={arrowDownCss} />
+        <Box
+          width="0px"
+          height="0px"
+          borderTop="50px solid #2b6cb0"
+          borderLeft="50px solid transparent"
+          borderRight="50px solid transparent"
+          position="absolute"
+          left="50%"
+          top={["30px", "60px", "90px"]}
+          transform="translate(-50%, 0)"
+          zIndex="4"
+        />
       </Box>
+      <Box
+        background="white"
+        height={["10px", "20px", "30px"]}
+        width="100%"
+        position="relative"
+      >
+        <Box
+          width="0px"
+          height="0px"
+          borderTop="55px solid white"
+          borderLeft="55px solid transparent"
+          borderRight="55px solid transparent"
+          position="absolute"
+          left="50%"
+          top={["10px", "20px", "30px"]}
+          transform="translate(-50%, 0)"
+          zIndex="2"
+        />
+      </Box>
+      <Box
+        background="#2D3748"
+        height={["10px", "20px", "30px"]}
+        width="100%"
+        position="relative"
+      >
+        <Box
+          width="0px"
+          height="0px"
+          borderTop="60px solid #2D3748"
+          borderLeft="60px solid transparent"
+          borderRight="60px solid transparent"
+          position="absolute"
+          left="50%"
+          top={["10px", "20px", "30px"]}
+          transform="translate(-50%, 0)"
+        />
+      </Box>
+      <Image src={BackgroundImage} />
     </Flex>
   );
 }
