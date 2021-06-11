0e5b0e5a522e701d11cc38932ecd9303e0395b7d
Javier Ocampo <ferroxido@gmail.com>
2021-06-10 23:25:14 -0300

    WIP: Team section

--- src/components/Team/index.tsx
+++ src/components/Team/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Box, Flex, Image } from '@chakra-ui/react';
 import BackgroundImage from '../../assets/panel.jpg';
+import Background2Image from '../../assets/gate.jpg';
 
 function Team(): JSX.Element {
   return (
     <Flex direction="column">
       <Box
         background="blue.600"
-        height={["30px", "60px", "90px"]}
+        height={["30px", "40px", "60px"]}
         width="100%"
         position="relative"
       >
@@ -19,7 +20,7 @@ function Team(): JSX.Element {
           borderRight="50px solid transparent"
           position="absolute"
           left="50%"
-          top={["30px", "60px", "90px"]}
+          top={["30px", "40px", "60px"]}
           transform="translate(-50%, 0)"
           zIndex="4"
         />
@@ -61,7 +62,44 @@ function Team(): JSX.Element {
           transform="translate(-50%, 0)"
         />
       </Box>
-      <Image src={BackgroundImage} />
+      <Image src={BackgroundImage} maxHeight="400px" objectFit="cover" />
+      <Box
+        background="#2D3748"
+        height={["15px", "15px", "20px"]}
+        width="100%"
+        position="relative"
+      >
+        <Box
+          width="150px"
+          height="0px"
+          borderBottom="25px solid #2D3748"
+          borderLeft="25px solid transparent"
+          borderRight="25px solid transparent"
+          position="absolute"
+          left="50%"
+          bottom={["15px", "15px", "20px"]}
+          transform="translate(-50%, 0)"
+        />
+      </Box>
+      <Box
+        background="#A0AEC0"
+        height={["10px", "20px", "30px"]}
+        width="100%"
+        position="relative"
+      >
+        <Box
+          width="150px"
+          height="0px"
+          borderBottom="25px solid #A0AEC0"
+          borderLeft="30px solid transparent"
+          borderRight="30px solid transparent"
+          position="absolute"
+          left="50%"
+          bottom={["10px", "20px", "30px"]}
+          transform="translate(-50%, 0)"
+          zIndex="4"
+        />
+      </Box>
     </Flex>
   );
 }
