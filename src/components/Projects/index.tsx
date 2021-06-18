3865dd3ae26af4c9cfed9daef273bde76391f564
Javier Ocampo <ferroxido@gmail.com>
2021-06-18 20:36:47 -0300

    WIP: projects section

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -3,7 +3,7 @@ import { Box, Center, Flex, Image, VStack, HStack, Text } from '@chakra-ui/react
 
 function Projects(): JSX.Element {
   return (
-    <Center bg="gray.700">
+    <Center bg="blue.900">
       <Flex
         maxWidth="1024px"
         wrap="wrap"
@@ -15,11 +15,18 @@ function Projects(): JSX.Element {
         border="2px solid white"
       >
         <Box width={["100%", "100%", "60%"]}>
-          <Image src="https://picsum.photos/800/600" borderRadius="8px" />
+          <Image src="https://picsum.photos/800/600" borderRadius={["8px 8px 0px 0px", "8px 8px 0px 0px", "8px"]} />
         </Box>
-        <Flex direction="column" wrap="wrap" width={["100%", "100%", "38%"]}>
-          <Text variant="xl" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat velit alias et dicta tempora culpa, voluptate ab blanditiis esse eius itaque dolore asperiores sunt, tenetur enim commodi exercitationem in. Dolorum expedita provident aut quas dolor aperiam amet assumenda placeat! Ea impedit, cum rerum ducimus dignissimos odio amet! Officiis, minus cupiditate?</Text>
-          <Text variant="xl" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat velit alias et dicta tempora culpa, voluptate ab blanditiis esse eius itaque dolore asperiores sunt, tenetur enim commodi exercitationem in. Dolorum expedita provident aut quas dolor aperiam amet assumenda placeat! Ea impedit, cum rerum ducimus dignissimos odio amet! Officiis, minus cupiditate?</Text>
+        <Flex
+          direction="column"
+          wrap="wrap"
+          width={["100%", "100%", "38%"]}
+          bg="white"
+          borderRadius={["0px 0px 4px 4px", "0px 0px 4px 4px", "4px"]}
+          padding="8px"
+        >
+          <Text fontSize="md" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat velit alias et dicta tempora culpa, voluptate ab blanditiis esse eius itaque dolore asperiores sunt, tenetur enim commodi exercitationem in. Dolorum expedita provident aut quas dolor aperiam amet assumenda placeat! Ea impedit, cum rerum ducimus dignissimos odio amet! Officiis, minus cupiditate?</Text>
+          <Text fontSize="md" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat velit alias et dicta tempora culpa, voluptate ab blanditiis esse eius itaque dolore asperiores sunt, tenetur enim commodi exercitationem in. Dolorum expedita provident aut quas dolor aperiam amet assumenda placeat! Ea impedit, cum rerum ducimus dignissimos odio amet! Officiis, minus cupiditate?</Text>
         </Flex>
       </Flex>
     </Center>
