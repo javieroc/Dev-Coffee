6d5b0e71df5fbd5c299166dbae90352b7cfbecb6
Javier Ocampo <ferroxido@gmail.com>
2021-06-18 23:59:38 -0300

    WIP: projects section

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -15,19 +15,19 @@ function Projects(): JSX.Element {
         border="2px solid white"
       >
         <Box width={["100%", "100%", "60%"]}>
-          <Image src="https://picsum.photos/800/600" borderRadius={["8px 8px 0px 0px", "8px 8px 0px 0px", "8px"]} />
+          <Image
+            src="https://picsum.photos/800/600"
+            borderRadius={["8px 8px 0px 0px", "8px 8px 0px 0px", "8px"]}
+          />
         </Box>
-        <Flex
-          direction="column"
-          wrap="wrap"
+        <Box
           width={["100%", "100%", "38%"]}
           bg="white"
           borderRadius={["0px 0px 4px 4px", "0px 0px 4px 4px", "4px"]}
-          padding="8px"
+          padding="16px"
         >
-          <Text fontSize="md" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat velit alias et dicta tempora culpa, voluptate ab blanditiis esse eius itaque dolore asperiores sunt, tenetur enim commodi exercitationem in. Dolorum expedita provident aut quas dolor aperiam amet assumenda placeat! Ea impedit, cum rerum ducimus dignissimos odio amet! Officiis, minus cupiditate?</Text>
-          <Text fontSize="md" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat velit alias et dicta tempora culpa, voluptate ab blanditiis esse eius itaque dolore asperiores sunt, tenetur enim commodi exercitationem in. Dolorum expedita provident aut quas dolor aperiam amet assumenda placeat! Ea impedit, cum rerum ducimus dignissimos odio amet! Officiis, minus cupiditate?</Text>
-        </Flex>
+          <Text fontSize="lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat velit alias et dicta tempora culpa, voluptate ab blanditiis esse eius itaque dolore asperiores sunt, tenetur enim commodi exercitationem in. Dolorum expedita provident aut quas dolor aperiam amet assumenda placeat! Ea impedit, cum rerum ducimus dignissimos odio amet! Officiis, minus cupiditate?</Text>
+        </Box>
       </Flex>
     </Center>
   )
