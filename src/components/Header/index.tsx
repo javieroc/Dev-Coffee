448f6ad2d49baf63ee61ffd5776673637c910540
Javier Ocampo <ferroxido@gmail.com>
2021-07-11 12:38:45 -0300

    Animate hero background image

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Box } from '@chakra-ui/react';
 import { Navbar } from '../Navbar';
+import { AnimatePresence, motion } from 'framer-motion';
+import { css } from '@emotion/css';
+import Morning from '../../assets/morning-1.jpg';
+import Afternoon from '../../assets/afternoon-1.jpg';
+import Night from '../../assets/night-1.jpg';
 
-const images = import.meta.globEager('/src/assets/*.jpg');
+const imageCss = css({
+  position: 'absolute',
+  width: '100%',
+  height: '35vw',
+});
+
+const variants = {
+  enter: {
+    opacity: 0
+  },
+  center: {
+    zIndex: 1,
+    opacity: 1
+  },
+  exit: {
+    zIndex: 0,
+    opacity: 0
+  }
+};
 
 function Header(): JSX.Element {
+  const images: string[] = [Morning, Afternoon, Night];
+  const [image, setImage] = useState<number>(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => setImage((image + 1) % 3), 10000);
+    return () => clearInterval(intervalId);
+  }, [image, setImage]);
+
   return (
-    <Box
-      background={`url(${images['/src/assets/morning-1.jpg'].default}) no-repeat`}
-      backgroundPosition=""
-      backgroundSize="100% 100%, cover"
-      minHeight="35vw"
-    >
+    <Box minHeight="35vw" >
+      <AnimatePresence>
+        <motion.img
+          key={images[image]}
+          className={imageCss}
+          src={images[image]}
+          initial="enter"
+          animate="center"
+          exit="exit"
+          variants={variants}
+          transition={{ duration: 2 }}
+        />
+      </AnimatePresence>
       <Navbar />
     </Box>
   )
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,4 +1,4 @@
-import { Flex, Text } from '@chakra-ui/react';
+import { Box, Flex, Text } from '@chakra-ui/react';
 import React from 'react';
 import { useAppContext } from '../../context';
 
@@ -11,71 +11,73 @@ function Navbar(): JSX.Element {
   } = useAppContext()!;
 
   return (
-    <Flex
-      align="center"
-      justify="space-between"
-      padding="1em"
-    >
-      <Flex bg="rgb(247,250,252, .85)" padding="4px" borderRadius="4px">
-        <Text fontSize="xl" color="orange.400">Dev</Text>
-        <Text fontSize="xl" color="gray.400">&</Text>
-        <Text fontSize="xl" color="black">Coffee</Text>
+    <Box position="absolute" zIndex="10" width="100%">
+      <Flex
+        align="center"
+        justify="space-between"
+        padding="1em"
+      >
+        <Flex bg="rgb(247,250,252, .85)" padding="4px" borderRadius="4px">
+          <Text fontSize="xl" color="orange.400">Dev</Text>
+          <Text fontSize="xl" color="gray.400">&</Text>
+          <Text fontSize="xl" color="black">Coffee</Text>
+        </Flex>
+        <Flex>
+          <Text
+            border="2px solid rgb(247,250,252, .85)"
+            borderRadius="4px"
+            padding="4px"
+            color="rgb(247,250,252, .85)"
+            margin="0 4px"
+            cursor="pointer"
+            onClick={() => {
+              stackSectionRef.current?.scrollIntoView({ behavior: 'smooth' })
+            }}
+          >
+            Stack
+          </Text>
+          <Text
+            border="2px solid rgb(247,250,252, .85)"
+            borderRadius="4px"
+            padding="4px"
+            color="rgb(247,250,252, .85)"
+            margin="0 4px"
+            cursor="pointer"
+            onClick={() => {
+              teamSectionRef.current?.scrollIntoView({ behavior: 'smooth' })
+            }}
+          >
+            Team
+          </Text>
+          <Text
+            border="2px solid rgb(247,250,252, .85)"
+            borderRadius="4px"
+            padding="4px"
+            color="rgb(247,250,252, .85)"
+            margin="0 4px"
+            cursor="pointer"
+            onClick={() => {
+              projectsSectionRef.current?.scrollIntoView({ behavior: 'smooth' })
+            }}
+          >
+            Projects
+          </Text>
+          <Text
+            border="2px solid rgb(247,250,252, .85)"
+            borderRadius="4px"
+            padding="4px"
+            color="rgb(247,250,252, .85)"
+            margin="0 4px"
+            cursor="pointer"
+            onClick={() => {
+              contactUsSectionRef.current?.scrollIntoView({ behavior: 'smooth' })
+            }}
+          >
+            Contact Us
+          </Text>
+        </Flex>
       </Flex>
-      <Flex>
-        <Text
-          border="2px solid rgb(247,250,252, .85)"
-          borderRadius="4px"
-          padding="4px"
-          color="rgb(247,250,252, .85)"
-          margin="0 4px"
-          cursor="pointer"
-          onClick={() => {
-            stackSectionRef.current?.scrollIntoView({ behavior: 'smooth' })
-          }}
-        >
-          Stack
-        </Text>
-        <Text
-          border="2px solid rgb(247,250,252, .85)"
-          borderRadius="4px"
-          padding="4px"
-          color="rgb(247,250,252, .85)"
-          margin="0 4px"
-          cursor="pointer"
-          onClick={() => {
-            teamSectionRef.current?.scrollIntoView({ behavior: 'smooth' })
-          }}
-        >
-          Team
-        </Text>
-        <Text
-          border="2px solid rgb(247,250,252, .85)"
-          borderRadius="4px"
-          padding="4px"
-          color="rgb(247,250,252, .85)"
-          margin="0 4px"
-          cursor="pointer"
-          onClick={() => {
-            projectsSectionRef.current?.scrollIntoView({ behavior: 'smooth' })
-          }}
-        >
-          Projects
-        </Text>
-        <Text
-          border="2px solid rgb(247,250,252, .85)"
-          borderRadius="4px"
-          padding="4px"
-          color="rgb(247,250,252, .85)"
-          margin="0 4px"
-          cursor="pointer"
-          onClick={() => {
-            contactUsSectionRef.current?.scrollIntoView({ behavior: 'smooth' })
-          }}
-        >
-          Contact Us
-        </Text>
-      </Flex>
-    </Flex>
+    </Box>
   )
 }
 
