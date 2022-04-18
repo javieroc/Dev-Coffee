8e451f1c90b0710b16a4685707158259c0aad39a
Javier Ocampo <ferroxido@gmail.com>
2022-04-18 18:30:33 -0300

    change footer background color

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,7 @@ import { Flex, Text } from '@chakra-ui/react';
 
 function Footer(): JSX.Element {
   return (
-    <Flex bg="gray.700" padding="1em" justifyContent="flex-end">
+    <Flex bg="gray.800" padding="1em" justifyContent="flex-end">
       <Text
         fontSize="md"
         as="i"
